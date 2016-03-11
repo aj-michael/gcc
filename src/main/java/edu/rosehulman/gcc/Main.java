42172ed4f9ee73375a16d9e9ee3f29d1093bffec
Adam Michael <adam@ajmichael.net>
2016-03-10 21:30:53 -0500

    Switched runtime java cup dependency.

--- src/main/java/edu/rosehulman/gcc/Main.java
+++ src/main/java/edu/rosehulman/gcc/Main.java
@@ -1,29 +1,23 @@
 package edu.rosehulman.gcc;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
+
 import edu.rosehulman.gcc.token.Token;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         for(String arg : args) {
             File file = new File(arg);
-            try {
-                Scanner scanner = new Scanner(file);
-                String text = "";
-                while (scanner.hasNextLine()){
-                    text += scanner.nextLine();
-                }
-                Lexer lexer = new Lexer(text);
-                ImmutableList<Token> tokens = lexer.getTokens();
-                for(Token token : tokens) {
-                    System.out.println(token);
-                }
-            } catch (FileNotFoundException e) {
-                System.out.println("File " + arg + " was not found.");
+            String text = Files.toString(file, Charsets.UTF_8).trim();
+            Lexer lexer = new Lexer(text);
+            ImmutableList<Token> tokens = lexer.getTokens();
+            for(Token token : tokens) {
+                System.out.println(token);
             }
         }
     }
