e17549df67a225f43b20210d4c448d3b307cc6bf
Adam Michael <adam@ajmichael.net>
2016-03-13 15:38:09 -0400

    Cleaned up testing code.

--- src/test/java/edu/rosehulman/minijavac/LexerTest.java
+++ src/test/java/edu/rosehulman/minijavac/LexerTest.java
@@ -8,13 +8,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -24,65 +21,22 @@ public class LexerTest {
 
     private static final String INPUT_FOLDER = "src/test/resources/lexer/java";
     private static final String OUTPUT_FOLDER = "src/test/resources/lexer/out";
-
-    static class DirectoryFilter implements FileFilter {
-
-        boolean wantDirectories;
-
-        public DirectoryFilter(boolean wantDirectories) {
-            this.wantDirectories = wantDirectories;
-        }
-
-        @Override
-        public boolean accept(File pathname) {
-            return this.wantDirectories == pathname.isDirectory();
-        }
-    }
-
-    public static List<File> findFiles(File file) {
-        if(file.isDirectory()) {
-            File[] files = file.listFiles(new DirectoryFilter(false));
-            File[] directories = file.listFiles(new DirectoryFilter(true));
-            List<File> fileList = Arrays.asList(files);
-            for(File directory : directories) {
-                fileList.addAll(LexerTest.findFiles(directory));
-            }
-            return fileList;
-        } else {
-            List<File> files = new ArrayList<File>(1);
-            files.add(file);
-            return files;
-        }
-    }
+    private static final String OUTPUT_EXTENSION = ".out";
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<File[]> testCases() {
-        File inputFolder = new File(INPUT_FOLDER);
-        File outputFolder = new File(OUTPUT_FOLDER);
-
-        List<File> inputFiles = findFiles(inputFolder);
-        List<File> outputFiles = findFiles(outputFolder);
-
-        Collections.sort(inputFiles);
-        Collections.sort(outputFiles);
-
-        if(inputFiles.size() != outputFiles.size()) {
-            return null;
-        }
-
         List<File[]> fileCollection = new ArrayList<>();
-        for(int k = 0; k < inputFiles.size(); k++) {
-            File[] filePair = new File[2];
-            filePair[0] = inputFiles.get(k);
-            filePair[1] = outputFiles.get(k);
+        for (File testFile : Files.fileTreeTraverser().children(new File(INPUT_FOLDER))) {
+            File outputFile = new File(OUTPUT_FOLDER,
+                    Files.getNameWithoutExtension(testFile.getName()) + OUTPUT_EXTENSION);
+            File[] filePair = {testFile, outputFile};
             fileCollection.add(filePair);
         }
-
         return fileCollection;
     }
 
-    private File testFile;
-    private File outputFile;
+    private final File testFile;
+    private final File outputFile;
 
     public LexerTest(File testFile, File outputFile) {
         this.testFile = testFile;
