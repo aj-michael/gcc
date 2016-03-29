dc650de80e5669acff9c1354260c46672de4c458
Adam Michael <adam@ajmichael.net>
2016-03-29 19:52:02 -0400

    Completed parser.

--- src/main/java/edu/rosehulman/minijavac/Main.java
+++ src/main/java/edu/rosehulman/minijavac/Main.java
@@ -2,20 +2,13 @@ package edu.rosehulman.minijavac;
 
 import edu.rosehulman.minijavac.generated.Lexer;
 import edu.rosehulman.minijavac.generated.Parser;
-import edu.rosehulman.minijavac.generated.Symbols;
-import java_cup.runtime.ComplexSymbolFactory;
-import java_cup.runtime.Symbol;
 
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Main {
     public static void main(String args[]) throws Exception {
-        ComplexSymbolFactory csf = new ComplexSymbolFactory();
-        Lexer lexer = new Lexer(new FileReader("src/test/resources/parser/java/testcase04_04.java"));
-        for (Symbol symbol : lexer) {
-            System.out.println(Symbols.terminalNames[symbol.sym]);
-        }
+        Parser parser = new Parser(new Lexer(new FileReader(args[0])));
+        parser.parse();
+        System.out.println(parser.getParseLog());
     }
 }
