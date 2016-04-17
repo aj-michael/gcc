8ed1d67d30e98bd1f0913097fb7a0cbf8bc6f128
Adam Michael <adam@ajmichael.net>
2016-04-16 21:41:10 -0400

    Created TypeChecker executable.

--- src/main/java/edu/rosehulman/minijavac/Main.java
+++ src/main/java/edu/rosehulman/minijavac/Main.java
@@ -3,14 +3,16 @@ package edu.rosehulman.minijavac;
 import edu.rosehulman.minijavac.ast.Program;
 import edu.rosehulman.minijavac.generated.Lexer;
 import edu.rosehulman.minijavac.generated.Parser;
+import edu.rosehulman.minijavac.typechecker.TypeChecker;
 
 import java.io.FileReader;
 
 public class Main {
     public static void main(String args[]) throws Exception {
-        Parser parser = new Parser(new Lexer(new FileReader("src/test/resources/parser/java/testcase00_09.java")));
+        Parser parser = new Parser(new Lexer(new FileReader(args[0])));
         Program program = parser.parseProgram();
-        System.out.println(parser.getParseLog());
-        System.out.println(program);
+        TypeChecker typechecker = new TypeChecker();
+        typechecker.isValid(program);
+        System.out.println(typechecker.getTypeCheckerLog());
     }
 }
