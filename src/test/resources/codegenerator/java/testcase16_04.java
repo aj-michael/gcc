0fe73e8f143a1322bfc615296e33b21500316901
David Robinson <robinsdm@rose-hulman.edu>
2016-05-14 22:36:33 -0400

    Added double, long, float test

---
+++ src/test/resources/codegenerator/java/testcase16_04.java
@@ -0,0 +1,18 @@
+class test {
+    public static void main(String[] args) {
+        float a = 0.5f;
+        float b = 1.0f;
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(a * b);
+        System.out.println(b / 5.0f);
+        System.out.println(a > b);
+        System.out.println(a == b);
+        System.out.println(a == a);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a <= a);
+        System.out.println(a >= a);
+    }    
+}
---
+++ src/test/resources/codegenerator/java/testcase16_05.java
@@ -0,0 +1,18 @@
+class test {
+    public static void main(String[] args) {
+        long a = 50L;
+        long b = 100L;
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(a * b);
+        System.out.println(b / 5L);
+        System.out.println(a > b);
+        System.out.println(a == b);
+        System.out.println(a == a);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a <= a);
+        System.out.println(a >= a);
+    }    
+}
